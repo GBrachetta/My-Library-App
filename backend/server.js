6ade6e9dc5b5d742f15772b550b43dfa17787f8e
Guillermo Brachetta <brachetta@me.com>
2022-02-01 15:47:22 +0100

    Setup backend authentication

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+// eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+
+const connectDB = require('./config/db');
+const userRouter = require('./routes/userRoutes');
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
 const { PORT } = process.env;
@@ -13,18 +19,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
+app.use('/api/users', userRouter);
+
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'),
   );
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to the Guillermo's Library API" });
   });
 }
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
