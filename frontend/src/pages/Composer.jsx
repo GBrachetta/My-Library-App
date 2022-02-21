6f6e271fec5b90b20a625cc65f85ed029d2eafd1
Guillermo Brachetta <brachetta@me.com>
2022-02-21 08:45:42 +0100

    Add books by composer to individual composer page.

--- frontend/src/pages/Composer.jsx
+++ frontend/src/pages/Composer.jsx
@@ -1,16 +1,86 @@
+import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import BackButton from '../components/BackButton';
+import BookItem from '../components/BookItem';
 import Spinner from '../components/Spinner';
 import Title from '../components/Title';
 import { getComposer, reset } from '../features/composers/composerSlice';
+import { useFuzzySearch } from '../hooks/useFuzzySearch';
+
+const ComposerData = ({ composer, booksByComposer }) => {
+  const fuzzyOptions = {
+    keys: ['title', 'setting'],
+  };
+
+  const { searchResults, setSearchTerm, searchTerm } = useFuzzySearch({
+    data: booksByComposer,
+    fuzzyOptions,
+  });
+
+  const books = searchTerm
+    ? searchResults.map((book) => (
+        <BookItem key={book._id} book={book} noComposer />
+      ))
+    : []
+        .concat(booksByComposer)
+        .sort((a, b) => (a.title > b.title ? 1 : -1))
+        .map((book) => <BookItem key={book._id} book={book} noComposer />);
+
+  return (
+    <>
+      <section className="grid place-items-center border-b-2 ">
+        <p className="text-l text-gray-400 py-3">
+          FILTER BY TITLE, COMPOSER, SETTING OR COUNTRY
+        </p>
+        <input
+          className="text-gray-300 mb-5 form-input"
+          type="search"
+          placeholder="Search..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </section>
+      <div>
+        <p className="text-info text-center my-5 text-3xl font-semibold border-b-2 border-gray-600 pb-5">
+          {composer.names || null} {composer.surname}
+          {composer.country ? ', ' : ''} {composer.country || null}
+          {composer.born ? ', ' : ''} {composer.born || '?'} -{' '}
+          {composer.died || '?'}
+        </p>
+      </div>
+      <section className="table w-full mb-5">
+        <div className="table-header-group my-5 border-b-2 border-2">
+          <div className="table-row ">
+            <div className="table-cell border-b py-5 border-gray-600 font-bold">
+              Title
+            </div>
+            <div className="table-cell border-b py-5 border-gray-600 font-bold">
+              Subtitle
+            </div>
+            <div className="table-cell border-b py-5 border-gray-600 font-bold">
+              Setting
+            </div>
+            <div className="table-cell border-b py-5 border-gray-600" />
+          </div>
+        </div>
+        <div className="table-row-group border-collapse border">
+          {books.length ? (
+            books
+          ) : (
+            <p className="mt-5">No books by this composer</p>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
 const Composer = () => {
   const {
-    composer: { surname, names, country, born, died },
+    composer: { composer, booksByComposer },
     isLoading,
     isError,
     message,
@@ -37,25 +107,11 @@ const Composer = () => {
     <>
       <Title title="Composer" />
 
+      {composer && (
+        <ComposerData composer={composer} booksByComposer={booksByComposer} />
+      )}
+
       <section>
-        <div>
-          <p className="text-info text-center my-5 text-3xl font-semibold border-b-2 border-gray-600 pb-5">
-            {names || null} {surname}
-            {country ? ', ' : ''} {country || null}
-            {born ? ', ' : ''} {born || '?'} - {died || '?'}
-          </p>
-        </div>
-
-        <section className="my-5">
-          <div>Books</div>
-          <div>Books</div>
-          <div>Books</div>
-          <div>Books</div>
-          <div>Books</div>
-          <div>Books</div>
-          <div>Books</div>
-        </section>
-
         <div className="text-center border-t-2 border-gray-600">
           <p className="font-semibold text-info mt-5">
             Add a book by this composer
@@ -73,6 +129,15 @@ const Composer = () => {
       <BackButton url="/composers" />
     </>
   );
+};
+
+ComposerData.defaultProps = {
+  booksByComposer: [],
+};
+
+ComposerData.propTypes = {
+  composer: PropTypes.object.isRequired,
+  booksByComposer: PropTypes.array,
 };
 
 export default Composer;
